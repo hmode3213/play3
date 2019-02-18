73def82c7de765a3cf1a6f8dd6abe278cdf9c3af
hmode3213 <47697661+hmode3213@users.noreply.github.com>
2019-02-18 03:41:24 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NTQ2MzUwNDY4ODcyNTM2MDg0.D0uQHg.ln2vAtBWQs0XO_Pw8v1KOW_Pez4', 
+    YT_API_KEY: 'AIzaSyCQapWI28_Um0n4AEGNVw7l3QZqhQF0cFs', 
+    prefix: '3',
+    devs: ['457056843986894865']
+}
